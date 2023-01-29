7f59d8efdc20eb2e84c0d830300919fc0799f091
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-01-30 00:36:52 +0100

    Övning 5.3.5

---
+++ Kap 5/5.3.5/index.js
@@ -0,0 +1,41 @@
+
+const random_number_request = new Request("https://teaching.maumt.se/apis/random_number/");
+
+
+let counter = 0;
+        document.querySelector("#number1").textContent = "...";
+        document.querySelector("#number2").textContent = "...";
+        document.querySelector("#sum").textContent = "waiting";
+document.querySelector("button").addEventListener("click", activate)
+
+
+function activate (event) {
+counter = 0;
+
+//number 1
+fetch(random_number_request)
+    .then(r => r.text())
+    .then(resource => {
+        document.querySelector("#number1").textContent = resource;
+        counter++;
+        if(counter === 2){
+            sum_function();
+        };
+    });
+
+//number 2
+fetch(random_number_request)
+    .then(r => r.text())
+    .then(resource => {
+        document.querySelector("#number2").textContent = resource;
+        counter++;
+        if(counter === 2){
+            sum_function();
+        };
+    });
+}
+
+
+function sum_function(){
+    document.querySelector("#sum").textContent=parseInt(document.querySelector("#number1").textContent) + parseInt(document.querySelector("#number2").textContent);
+}
