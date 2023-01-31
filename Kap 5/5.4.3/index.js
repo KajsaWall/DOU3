cb8f55fafbdc5c11e18c97c6279d29384c693a6e
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-01-31 16:34:08 +0100

    Övning 5.4.3

---
+++ Kap 5/5.4.3/index.js
@@ -0,0 +1,9 @@
+/*
+
+I denna övning ska ni använda resursen
+https://teaching.maumt.se/apis/random_color/
+
+Tänk noggrannt hur ni ska koda CSS innan ni sätter igång.
+Använd papper och penna för att tänka bättre.
+
+*/
