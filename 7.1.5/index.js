8fa091b87678a2e9cafc045e8d1587e21e46502a
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-02-04 15:06:28 +0100

    fgd

---
+++ 7.1.5/index.js
@@ -0,0 +1,22 @@
+/*
+
+Samma problem som unit 6.2.4 (alltså övning 3 i sektionen 6.2).
+
+Dessa resurser finns (jag har lagt till en längre väntetid så ni bättre hinner se vad som händer):
+https://teaching.maumt.se/apis/attractions/?t_min=2&attraction=X
+https://teaching.maumt.se/apis/attractions/?t_min=2&city=Y
+https://teaching.maumt.se/apis/attractions/?t_min=2&country=Z
+
+Där X måste vara en av följande: Lavapies, Templo_de_Debod, Fontana_di_trevi, Pantheon, Brandenburger_Tor eller Kreuzberg.
+Notera underscore i namnen på sevärdheterna!
+
+Y måste vara någon av följande: Madrid, Rome eller Berlin.
+
+Z måste vara någon av följande: Spain, Italy, Germany
+
+Koda en sida som fungerar som den på videon. Du ska koda två lösningar:
+
+(1) I den första lösning får inte .then förekomma.
+(2) I den andra får inte async eller await förekomma (ja, som ni löste den i sektionen 6.2. Kan du fixa det igen utan att kolla på lösningen?)
+
+*/
