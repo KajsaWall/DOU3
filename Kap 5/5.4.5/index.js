62cd933fb96f8440e0b0f09a54c7f74ec490592e
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-02-11 00:22:42 +0100

    Övning

--- Kap 5/5.4.5/index.js
+++ Kap 5/5.4.5/index.js
@@ -16,4 +16,39 @@ genom att fetcha första resursen så många gånger som det behövs.
 Eftersom det är 147 färger så har jag skapat en grid på 21 rader och 7 kolumner.
 
 */
+const list_request = new Request("https://teaching.maumt.se/apis/random_color/?all&list");
 
+fetch(list_request)
+    .then(r => r.json())
+    .then(list => {
+        get_one_color(list, 1);
+    });
+
+function get_one_color (list, attempts) {
+    const request = new Request("https://teaching.maumt.se/apis/random_color/?all&bullet");
+    fetch(request)
+    .then(r => r.json())
+    .then(color => {
+        let index = list.indexOf(color);
+        if(index !== -1) {
+            make_color(color, list.length, attempts);
+            list.splice(index, 1);
+            attempts = 0;
+        }
+        if(list.length) {
+            attempts++;
+            get_one_color(list, attempts);
+        }
+    });
+}
+
+
+function make_color (color, count, attempts) {
+    const div = document.createElement("div");
+    div.style.backgroundColor = color;
+    div.textContent = `(${count}) ${color} (${attempts})`;
+  
+    // Append
+    document.getElementById("wrapper").appendChild(div);
+  }
+  
