c91606ac1725afc214eafb50147fd9a7fd0e1204
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-02-14 23:19:25 +0100

    10.2.3

--- 10.2.3/index.js
+++ 10.2.3/index.js
@@ -1,11 +1,21 @@
 /*
 
-Vi gör exakt samma sak som vi gjorde i förra övningar men denna
-gång ska vi dessutom hålla reda på hur många begäran som varje
-div behövde för att till slut få en färg.
+Vi fortsätter.
+
+Nu ska ni, när allting är klart (eller under tiden, som ni föredrar), visa på sidan
+hur många begäran vi behövde göra per div, i snitt.
+
+För er som verkligen inte gillar matte: snittet = summan av alla begäran / antalet divar
+
+I denna övningen får ni använda globala variabler men forstfarande samma anrop.
 
 */
 
+
+// Your global variables need to go before the start-up call (go()).
+// Otherwise they won't be of any use.
+let n_requests = 0;
+let n_filled_divs = 0;
 go(10, 10); // Argument: n_columns, n_rows
 
 function go (n_columns, n_rows) {
@@ -20,10 +30,19 @@ function go (n_columns, n_rows) {
 
 async function fill_color (div, n = 1) {
     try {
+    n_requests++;
     div.textContent = n;
     const response = await fetch("https://teaching.maumt.se/apis/random_color/");
     const color = await response.json();
+    n_filled_divs++;
     div.style.backgroundColor = color;
+
+    if(n_filled_divs === 100) {
+        document.getElementById("average").textContent = `
+        ${n_requests / 100} requests per div, in average
+        `;
+        document.getElementById("average").classList.add("visible");
+    }
 
     } catch (e) {
         if(e.message.includes("NetworkError")) {
