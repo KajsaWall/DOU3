db63599d6c3d44c26d5407526f78b5daefe0bcec
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-02-15 21:17:51 +0100

    sdfcs

---
+++ 10.2.4/index.js
@@ -0,0 +1,15 @@
+/*
+
+Samma som tidigare men ni får inte använda globala variabler.
+Tips: Jag har löst det genom att fuska då jag har använt mig
+      av HTML-element och deras textContent som ställen där
+      man spara information.
+
+Funktionen round() som finns deklarerad nedan kan användas
+för att säkerställa att en siffra alltid har exakt två decimaler.
+
+*/
+
+
+
+go(10, 10); // Argument: n_columns, n_rows
---
+++ 7.1.6/index.js
@@ -0,0 +1,24 @@
+/*
+
+Detta ger oss random siffror mellan 1 och 29:
+https://teaching.maumt.se/apis/random_number/?min=1&max=29&t_min=0&t_len=0.4
+(t_min och t_len ger oss snabbare resurser)
+
+Koda en sida som fungerar som den på videon. Krav:
+(1) .then får inte förekomma. Det måste alltså lösas med async och await
+(2) globala variabler får inte förekomma
+(3) siffrorna i brickorna måste komma i stigande ordning
+(4) allt ska starta med anropet som finns redan på plats: make_all_cards(16, 5);
+    där 16 är antalet kort och 5 antalet siffror i varje kort.
+
+
+Problemet inkluderar en (förhoppningsvis rimlig) CSS-utmaning.
+Om du tycker att den är för svår så:
+1) Kolla på lösningen så du kan lösa problemet
+2) Boka tid i din kalender för att gå igenom CSS från webb-kursen.
+   CSS:en på denna sida bör du kunna klara av utan större problem.
+
+*/
+
+
+make_all_cards(16, 5);
